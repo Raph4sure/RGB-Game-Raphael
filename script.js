fad9b889e0adfdb231adef96d225a5e2d513c8cb
Raph <raph4sure007@gmail.com>
2025-06-18 15:05:05 +0100

    add game controls and update styles for RGB Game

--- script.js
+++ script.js
@@ -1,26 +1,93 @@
-// const generate1 = Math.floor(Math.random() * 256);
+document
+    .querySelectorAll(".container, .reset, .displayRgb, .displayHeading")
+    .forEach((eachClass) => {
+        eachClass.classList.add("hide");
+    });
 
-// const generate2 = Math.floor(Math.random() * 256);
+document.querySelector(".start").addEventListener("click", function () {
+    document
+        .querySelectorAll(".container, .reset, .displayRgb, .displayHeading")
+        .forEach((eachClass) => {
+            eachClass.classList.remove("hide");
+        });
+});
 
-// const generate3 = Math.floor(Math.random() * 256);
+const genRandom = () => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
 
-// console.log(colors);
-
-const generate = () => Math.floor(Math.random() * 256);
-
-const changeColor = () => {
-    generate();
-
-    const colors = `rgb(${generate()} , ${generate()} , ${generate()})`;
-
-    const manipulate = (document.querySelector(".box").style.backgroundColor =
-        colors);
-
-    manipulate.addEventListener("click", changeColor);
-
-    console.log(colors);
+    return `rgb(${r}, ${g}, ${b})`;
 };
 
-// console.log(changeColor());
+console.log(genRandom());
 
-// console.dir(document.body)
+let allColors = [];
+let shuffleColor;
+let gameOver = false;
+const newGame = () => {
+    document.querySelectorAll(".box").forEach((manipulate) => {
+        changColor = genRandom();
+
+        manipulate.style.backgroundColor = changColor;
+
+        allColors.push(changColor);
+    });
+
+    console.log("all colors: ", allColors);
+
+    const randColor = allColors;
+
+    const randNo = Math.floor(Math.random() * allColors.length);
+
+    shuffleColor = allColors[randNo];
+
+    console.log("randNo: ", randNo);
+
+    console.log("shuffleColor: ", shuffleColor);
+
+    document.querySelector(
+        ".displayRgb"
+    ).textContent = `Rgb Value: ${shuffleColor}`;
+};
+
+newGame();
+
+document.querySelectorAll(".box").forEach((playGame, index) => {
+    playGame.addEventListener("click", function () {
+        if (gameOver) return;
+        // console.log('test')
+        let clickedCard = playGame.textContent;
+
+        clickedCard = allColors[index];
+
+        console.log("clickedCard: ", clickedCard);
+
+        console.log("allColors[index]: ", allColors[index]);
+
+        if (clickedCard === shuffleColor) {
+            playGame.textContent = "correct";
+
+            document.querySelectorAll(".box").forEach((singleBox) => {
+                singleBox.style.backgroundColor = allColors[index];
+            });
+            gameOver = true;
+        } else if (clickedCard !== shuffleColor) {
+            playGame.textContent = "wrong";
+            setTimeout(() => {
+                playGame.textContent = "?";
+            }, 300);
+        }
+    });
+});
+
+document.querySelector(".reset").addEventListener("click", function () {
+    allColors = [];
+    newGame();
+
+    document.querySelectorAll(".box").forEach((box) => {
+        box.textContent = "?";
+    });
+
+    gameOver = false;
+});
